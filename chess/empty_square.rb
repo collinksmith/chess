059ab08ed3a7063e9c6ff1bc94109652de1850ba
Collin Smith <collink33@gmail.com>
2015-06-30 16:10:37 -0700

    Add EmptySquare as null object

--- chess/empty_square.rb
+++ chess/empty_square.rb
@@ -5,4 +5,20 @@ class EmptySquare
   def to_s
     '   '
   end
+
+  def empty?
+    true
+  end
+
+  def color
+    nil
+  end
+
+  def valid_move?(end_pos)
+    false
+  end
+
+  def dup(board)
+    EmptySquare.new
+  end
 end
