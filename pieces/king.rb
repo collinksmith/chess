3dfb843e62d36e909c1ca3a0a880a4f18e09e82d
Collin Smith <collink33@gmail.com>
2015-08-28 13:03:23 -0700

    Add valid_castle?

--- pieces/king.rb
+++ pieces/king.rb
@@ -1,4 +1,5 @@
 require_relative 'piece'
+require 'byebug'
 
 class King < Piece
   MOVE_DIFFS = [[1, 1], [1, 0], [1, -1], [0, -1],
@@ -12,10 +13,33 @@ class King < Piece
   end
 
   def valid_move?(end_pos)
-    valid_step?(end_pos)
+    valid_step?(end_pos) || valid_castle?(end_pos)
   end
 
   def king?
+    true
+  end
+
+  private
+
+  def valid_castle?(end_pos)
+    home_row = pos[0]
+
+    # Check if position is occupied
+    return false if board[*end_pos].is_a?(Piece)
+
+    # Check is king has moved
+    return false if moved?
+
+    # Check if end pos is a valid position for a castle
+    return false unless end_pos[0] == home_row &&
+                        (end_pos[1] == 1 || end_pos[1] == 6)
+
+    # Check if the rook has moved
+    dir = pos[1] > 4 ? 1 : -1
+    rook = self.board[home_row, end_pos[1] + 1]
+    return false if rook.moved?
+
     true
   end
 end
